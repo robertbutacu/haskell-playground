556ccfe2f262ddaea7f120e31ee0f1d5f1c01dbb
robertbutacu <17516693+robertbutacu@users.noreply.github.com>
2019-06-25 23:07:01 +0100

    Applicative instance

--- CustomMaybe.hs
+++ CustomMaybe.hs
@@ -4,8 +4,13 @@ import Data.Functor
 import Control.Applicative
 import Control.Monad
 
-data CustomMaybe a = CustomNothing | CustomJust a
+data CustomMaybe a = CustomNothing | CustomJust a deriving (Show, Eq)
 
 instance Functor CustomMaybe where
     fmap f CustomNothing  = CustomNothing
     fmap f (CustomJust a) = CustomJust (f a)
+
+instance Applicative CustomMaybe where
+    pure a = CustomJust a
+    (<*>) f CustomNothing  = CustomNothing
+    (<*>) f (CustomJust a) = fmap (\ff -> ff a) f
