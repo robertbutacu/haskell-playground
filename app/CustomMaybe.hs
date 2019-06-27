22e95a0a962062c8a588d7400ebef807b0099ed5
robertbutacu <17516693+robertbutacu@users.noreply.github.com>
2019-06-27 15:14:39 +0100

    semigroup and monoid instances

--- app/CustomMaybe.hs
+++ app/CustomMaybe.hs
@@ -3,6 +3,8 @@ module CustomMaybe where
 import Data.Functor
 import Control.Applicative
 import Control.Monad
+import Data.Monoid
+import Data.Semigroup
 
 data CustomMaybe a = CustomNothing | CustomJust a deriving (Show, Eq)
 
@@ -11,7 +13,7 @@ instance Functor CustomMaybe where
     fmap f (CustomJust a) = CustomJust (f a)
 
 instance Applicative CustomMaybe where
-    pure a = CustomJust a
+    pure                   = CustomJust
     (<*>) f CustomNothing  = CustomNothing
     (<*>) f (CustomJust a) = fmap (\ff -> ff a) f
 
@@ -19,6 +21,17 @@ instance Monad CustomMaybe where
     (>>=) CustomNothing _  = CustomNothing
     (>>=) (CustomJust v) f = f v
 
+
+instance Semigroup a => Semigroup (CustomMaybe a) where
+    (<>) CustomNothing _ = CustomNothing
+    (<>) _ CustomNothing = CustomNothing
+    (<>) (CustomJust v1) (CustomJust v2) = CustomJust (v1 <> v2)
+
+instance Semigroup a => Monoid (CustomMaybe a) where
+    mempty = CustomNothing
+
+someOtherF :: Semigroup a => CustomMaybe a -> CustomMaybe a -> CustomMaybe a
+someOtherF cmA cmB = cmA <> cmB
 
 someF :: CustomMaybe Int
 someF = do x <- CustomJust 50
